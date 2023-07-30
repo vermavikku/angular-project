2a96067c43837dbf9146c73f1bbf3ff987776948
Vikas Verma <vermavikkudeveloper@gmail.com>
2023-07-30 17:57:22 +0530

    Json API Integration

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { SellerService } from '../services/seller.service';
+import {Router} from "@angular/router";
+import { signUp } from '../data-type';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css'],
 })
 export class SellerAuthComponent {
-  signUp(data: object): void {
+  constructor(private seller:SellerService,private router:Router){}
+  signUp(data: signUp): void {
     console.log(data);
+    this.seller.userSignUp(data).subscribe((result)=>{
+     if(result){
+      this.router.navigate(['seller-home']);
+     }
+    });
   }
 }
